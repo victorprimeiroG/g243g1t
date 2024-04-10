517403b1b6fba64bbf35015c518e41e28a6d163d
victorprimeiroG <110112719+victorprimeiroG@users.noreply.github.com>
2024-04-10 08:59:34 -0300

    Add files via upload

--- Nova pasta/g243g1t-main/script/script.js
+++ Nova pasta/g243g1t-main/script/script.js
@@ -13,6 +13,10 @@ function abrir(num) {
     if(num == "2"){
         document.getElementById("aba2").disabled = true;
         document.getElementById("a2").style.display = "block";
+        pegar_hora();
+        document.getElementById("hd").value = hd;
+        document.getElementById("md").value = md;
+        document.getElementById("sd").value = sd;
     }
     if(num == "3"){
         document.getElementById("aba3").disabled = true;
@@ -66,4 +70,28 @@ function contador(){
         inicio++;
         document.getElementById("valor").innerHTML = inicio;
     }
+}
+let hd = 0;
+let md = 0;
+let sd = 0;
+let alarme = null;
+function pegar_hora(){
+    let d = new Date();
+    hd = d.getHours();
+    md = d.getMinutes();
+    sd = d.getSeconds();
+}
+function ativar_alarme(){
+    let d = new Date();
+    if(hd==d.getHours() && md==d.getMinutes() && sd==d.getSeconds()){
+    clearInterval(alarme);
+    alert("Alarme... Voltando para Aba3");
+    abrir(3);
+   }
+}
+function ligar_alarme(){
+    hd = document.getElementById("hd").value;
+    md = document.getElementById("md").value;
+    sd = document.getElementById("sd").value;
+    alarme = setInterval(ativar_alarme,1000);
 }
